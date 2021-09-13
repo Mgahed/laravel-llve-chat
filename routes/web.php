bee1da5f5cb205b91677ee8da7dd9052338d8e10
Mgahed <abdelrhmanmgahed131@gmail.com>
2021-09-13 20:49:37 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-//    return view('index');
+    return view('index');
 });
 
 Route::post('send-message', function (\Illuminate\Http\Request $request) {
